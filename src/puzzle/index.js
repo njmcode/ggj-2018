8c349c9479c41293295b16d37876ad86dc0906ed
Neil McCallion <skipley@hotmail.com>
2018-01-26 21:19:40 +0000

    Basic puzzle proto

--- src/puzzle/index.js
+++ src/puzzle/index.js
@@ -27,27 +27,64 @@ let solutionArr = []
 let currentArr = []
 
 for (let i = 0; i < puzzleArrLen; i++) {
-  solutionArr.push(i)
-  currentArr.push(i)
+  const item = (i === puzzleArrLen - 1) ? null : i
+  solutionArr.push(item)
+  currentArr.push(item)
 }
+const solutionArrString = JSON.stringify(solutionArr)
 
 // Shuffle
 shuffle(currentArr)
-console.log(currentArr)
+console.log('puzzle', currentArr)
+let blankSpace = currentArr.indexOf(null)
+console.log('blank', blankSpace)
+
+// Handle click, swap items
+const handleClick = e => {
+  e.preventDefault;
+  const arrIndex = parseInt(e.srcElement.dataset.arrIndex);
+  console.log('clicked', arrIndex)
+
+  // Check adjacency to blank space
+  const isTopEdgeAdj = (arrIndex / PUZZLE_DIM) >> 0 === 0
+  const isLeftEdgeAdj = (arrIndex % PUZZLE_DIM) >> 0 === 0
+  const isBottomEdgeAdj = (arrIndex / PUZZLE_DIM) >> 0 === PUZZLE_DIM - 1
+  const isRightEdgeAdj = (arrIndex % PUZZLE_DIM) >>> 0 === PUZZLE_DIM - 1
+
+  if ((!isTopEdgeAdj && arrIndex - PUZZLE_DIM === blankSpace)
+    ||(!isLeftEdgeAdj && arrIndex - 1 === blankSpace)
+    ||(!isRightEdgeAdj && arrIndex + 1 === blankSpace)
+    ||(!isBottomEdgeAdj && arrIndex + PUZZLE_DIM === blankSpace)) {
+      // Move and re-render if possible
+      currentArr[blankSpace] = currentArr[arrIndex]
+      blankSpace = arrIndex
+      currentArr[blankSpace] = null
+      render()
+
+      // Check solution
+      if (JSON.stringify(currentArr) === solutionArrString) {
+        console.log('SOLVED')
+      }
+  }
+}
 
 // Render
 function render () {
   puzzleEl.innerHTML = ''
   for (let i = 0; i < puzzleArrLen; i++) {
     const piece = document.createElement('div')
-    if (i === 0) {
+    console.log(i, blankSpace)
+    if (currentArr[i] === null) {
+      // Null 'piece' is the blank space
       piece.classList.add(styles.puzzlePieceEmpty)
     } else {
       piece.classList.add(styles.puzzlePiece)
+      piece.dataset.pieceNum = currentArr[i]
       piece.textContent = currentArr[i].toString()
-      puzzleEl.appendChild(piece)
+      piece.addEventListener('click', handleClick, false)
     }
-
+    piece.dataset.arrIndex = i
+    puzzleEl.appendChild(piece)
   }
 }
 
