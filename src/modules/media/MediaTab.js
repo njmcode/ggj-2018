c3b10a9ea7b11aad80b853777a1b105c010b6956
Reuben Bradley <reuben.m.bradley@gmail.com>
2018-01-27 18:41:21 +0000

    Moved media tab prototype into new app structure

--- src/modules/media/MediaTab.js
+++ src/modules/media/MediaTab.js
@@ -1,4 +1,14 @@
-import template from './template'
+import styles from './media.css';
+import { hidden as hiddenCls } from 'shared/main.css';
+import template from './template';
+
+let photoList = [
+    'src/assets/location1.jpg',
+    'src/assets/location2.jpg',
+    'src/assets/location3.jpg',
+    'src/assets/location4.jpg',
+    'src/assets/location5.jpg'
+];
 
 class MediaTab {
   constructor() {
@@ -9,6 +19,51 @@ class MediaTab {
     this.emitter = emitter
 
     console.log('Media tab initialised')
+    this.mediaPanel = el.querySelector(`.${styles.mediaPanel}`) || el;
+    this.mediaDetail = el.querySelector(`.${styles.mediaDetail}`);
+    this.mediaDetail.querySelector('#detail-cancel').addEventListener('click', () => {
+        this.closeDetails();
+    });
+    this.mediaDetail.querySelector('#detail-submit').addEventListener('click', () => {
+        this.checkPasscode();
+    });
+    setTimeout(() => {
+        this.releasePhoto(0);
+        this.releasePhoto(1);
+    }, 0);
+  }
+
+  releasePhoto (index) {
+    if (index >= photoList.length) {
+        return;
+    }
+
+    const imgContainer = this.mediaPanel.querySelector(`[data-index="${index}"]`);
+    const imgEl = document.createElement('img');
+    imgEl.src = photoList[index];
+    imgEl.addEventListener('click', () => {
+        this.expandPhoto(imgEl);
+    });
+
+    imgContainer.classList.remove(styles.unavailable);
+    imgContainer.appendChild(imgEl);
+  }
+
+  expandPhoto (imgEl) {
+    this.mediaDetail.insertBefore(imgEl.cloneNode(false), this.mediaDetail.firstChild);
+    this.mediaDetail.classList.remove(hiddenCls);
+    console.log('expanding photo', hiddenCls);
+  }
+
+  closeDetails () {
+    this.mediaDetail.removeChild(this.mediaDetail.firstChild);
+    this.mediaDetail.classList.add(hiddenCls);
+  }
+
+  checkPasscode () {
+    // TODO: Check the passcode, return error if wrong
+    // If correct, close detail view and pass control to puzzle
+    this.closeDetails();
   }
 }
 
--- src/modules/media/template.js
+++ src/modules/media/template.js
@@ -1,4 +1,24 @@
+import styles from './media.css';
+import { hidden } from 'shared/main.css';
+
+let imgContainersTemplate = '';
+
+for (let i = 0; i < 9; i++) {
+    imgContainersTemplate += `<div data-index="${i}" class="${styles.imageThumbnail} ${styles.unavailable}"></div>`
+}
+
 export default `
-<div style="background:white;height:100%">
-  <span>Media tab goes here</span>
-</div>`
+<div class="${styles.mediaPanel}">
+    <div class="${styles.mediaList}">${imgContainersTemplate}</div>
+    <div class="${styles.mediaDetail} ${hidden}">
+        <div>
+            <label>Passcode:</label>
+            <input type="text" name="passcode">
+        </div>
+        <div>
+            <button id="detail-submit" type="button">Enter</button>
+            <button id="detail-cancel" type="button">Cancel</button>
+        </div>
+    </div>
+</div>
+`;
